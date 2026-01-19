cb52a04cb5a582a45b52ca7038a0c9599f51645e
Tecnologia <emesom.silva@outlook.com>
2026-01-19 13:42:16 -0300

    Fix: Enable CORS Credentials

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,6 +23,7 @@ app.use(
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(express.json());
